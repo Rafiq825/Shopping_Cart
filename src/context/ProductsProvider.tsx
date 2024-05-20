827197f4412e247c04cb08ef7814d029003c3c25
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-05-20 22:00:19 +0500

    added cartItems

--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -36,18 +36,18 @@ type ChildrenType = { children?: ReactElement | ReactElement[] }
 export const ProductsProvider = ({ children }: ChildrenType): ReactElement => {
     const [products, setProducts] = useState<ProductType[]>(initState)
 
-    useEffect(() => {
-        const fetchProducts = async (): Promise<ProductType[]> => {
-            const data = await fetch('http://localhost:3500/products').then(res => {
-                return res.json()
-            }).catch(err => {
-                if (err instanceof Error) console.log(err.message)
-            })
-            return data
-        }
+    // useEffect(() => {
+    //     const fetchProducts = async (): Promise<ProductType[]> => {
+    //         const data = await fetch('http://localhost:3500/products').then(res => {
+    //             return res.json()
+    //         }).catch(err => {
+    //             if (err instanceof Error) console.log(err.message)
+    //         })
+    //         return data
+    //     }
 
-        fetchProducts().then(products => setProducts(products))
-    }, [])
+    //     fetchProducts().then(products => setProducts(products))
+    // }, [])
 
     return (
         <ProductsContext.Provider value={{ products }}>
